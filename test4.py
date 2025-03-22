3132b80e02b512b9134e8232946ed87421c5700e
davyleroy <d.nkurunziz@alustudent.com>
2025-03-22 16:21:36 +0200

    Refactor Atari environment setup: consolidate code in test.py and remove unused files

--- test4.py
+++
@@ -1,8 +0,0 @@
-import os
-from ale_py import ALEInterface
-
-# Set the path to your ROMs directory
-os.environ["ALE_ROMS"] = "./roms"  # Replace with your ROM path
-
-# Initialize ALE (required for Gymnasium to detect Atari)
-ALEInterface()
